605a95cd4180e5c9bf58291c2a4ec9c1a096b59e
Gsilvabtec <gsilvabtec@gmail.com>
2026-08-17 17:07:15 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCvVkocze-dtZWoBNWFLIESD3sTslb1ifo",
@@ -16,4 +16,6 @@ export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true
+});
